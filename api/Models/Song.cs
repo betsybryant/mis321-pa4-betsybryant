e289c4794e1335d52430da0475a3a2b2362b7509
Betsy Bryant <betsybryant@Betsys-MacBook-Air.local>
2022-04-12 14:32:59 -0500

    testing

---
+++ api/Models/Song.cs
@@ -0,0 +1,11 @@
+namespace api.Models
+{
+    public class Song
+    {
+        public int Id {get; set;}
+        public string Title {get; set;}
+        public string DateAdded {get; set;}
+        public bool Deleted {get; set;}
+        
+    }
+}
